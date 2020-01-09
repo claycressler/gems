769ac94958c4147b0093c9a7eec41e4ad73a1294
claycressler <ccressler2@unl.edu>
2020-01-09 08:08:53 -0600

    verified that logistic_GEM and logistic_GEM_storage produce identical ecological and evolutionary dynamics

--- comparing_logistic_GEMs.R
+++ comparing_logistic_GEMs.R
@@ -1,3 +1,4 @@
+## The purpose of this script is to compare logistic_GEM, which just outputs the trait distribution every dt time steps, to logistic_GEM_storage, which stores every individual ever born, recording its time of birth, time of death, and parent's traits. This storage allows us to do things like look at parent-offspring regressions and, more importantly, to compute the fitness of every individual ever born to look at the relationship between traits and fitness.
 seed <- 10110
 tmax <- 200
 N0 <- 5
@@ -8,10 +9,10 @@ bs <- ds <- 0.02
 slope <- 0.3/1.8^2
 
 source("logistic_GEM.R")
-system.time(out1 <- logistic_GEM(seed, dt=0.1, tmax, N0, traitmean, traitcv, h2, bs, ds, slope))
+out1 <- logistic_GEM(seed, dt=0.1, tmax, N0, traitmean, traitcv, h2, bs, ds, slope)
 
 source("logistic_GEM_with_storage.R")
-system.time(out2 <- logistic_GEM_storage(seed, tmax, N0, traitmean, traitcv, h2, bs, ds, slope))
+out2 <- logistic_GEM_storage(seed, tmax, N0, traitmean, traitcv, h2, bs, ds, slope)
 
 ## Compare the dynamics
 time.seq <- seq(0, tmax, 0.1)
